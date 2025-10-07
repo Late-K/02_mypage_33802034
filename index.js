a7c906926ddc8e6b03d2a1d86ff9c6e99fd3bd96
Jeremi Kiermasz <kiermaszjeremi@gmail.com>
2025-10-07 09:23:12 +0100

    Updated message and added readme

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//MyPage web app
+
 var http = require("http");
 const port = 8000;
 
 http
   .createServer(function (req, res) {
-    console.log(res);
+    console.log(req);
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(
       "<!DOCTYPE html><html><head><title>My Page</title></head><body><h1>Hello World!</h1><p>This is my page.</p></body></html>"
